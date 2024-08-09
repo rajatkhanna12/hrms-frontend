4070f32d117e14292716f41400d2cfd790cef7cf
rajatkhanna <rajat.khanna@yirontechnologies.com>
2024-08-09 12:00:17 +0530

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/hrms-frontend/', // Set this to your repository name
   plugins: [react()],
 })
